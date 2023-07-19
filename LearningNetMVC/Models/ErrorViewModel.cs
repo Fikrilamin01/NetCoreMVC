f3bb508b0827dbbca40c41eb812ad9fcefd2c906
Fikrilamin01 <muhammadfikri.a@graduate.utm.my>
2023-07-19 14:36:22 +0800

    Add project files.

---
+++ LearningNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearningNetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
